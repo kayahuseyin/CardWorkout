9f7b9b13e80493aabd39d2ccdf01227fa59ab50d
Hüseyin Kaya <88154777+kayahuseyin@users.noreply.github.com>
2024-01-29 15:37:56 +0300

    Add Button Configurations

--- CardWorkout/CardSelectionVC.swift
+++ CardWorkout/CardSelectionVC.swift
@@ -8,10 +8,107 @@
 import UIKit
 
 class CardSelectionVC: UIViewController {
+    
+    let cardImageView = UIImageView()
+    let stopButton = CWButton(backgroundColor: .red, title: "Stop")
+    let resetButton = CWButton(backgroundColor: .systemGreen, title: "Reset")
+    let rulesButton = CWButton(backgroundColor: .systemBlue, title: "Rules")
 
+    var cards: [UIImage] = CardDeck.allValues
+    var timer: Timer!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+        configureUI()
+        startTimer()
+    }
+    
+    
+    func startTimer() {
+        timer = Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: #selector(showRandomCard), userInfo: nil, repeats: true)
         
-        view.backgroundColor = .red
+    }
+    
+    
+    @objc func stopTimer() {
+        timer.invalidate()
+    }
+    
+    
+    @objc func resetTimer() {
+        stopTimer()
+        startTimer()
+    }
+    
+    
+    @objc func showRandomCard() {
+        cardImageView.image = cards.randomElement()
+    }
+    
+    
+    private func configureUI() {
+        configureCardImageView()
+        configureStopButton()
+        configureResetButton()
+        configureRulesButton()
+    }
+    
+    
+    private func configureCardImageView() {
+        view.addSubview(cardImageView)
+        cardImageView.translatesAutoresizingMaskIntoConstraints = false
+        cardImageView.image = UIImage(named: "AS")
+        
+        NSLayoutConstraint.activate([
+            cardImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            cardImageView.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: -75),
+            cardImageView.widthAnchor.constraint(equalToConstant: 250),
+            cardImageView.heightAnchor.constraint(equalToConstant: 350)
+        ])
+    }
+    
+    
+    private func configureStopButton() {
+        view.addSubview(stopButton)
+        stopButton.addTarget(self, action: #selector(stopTimer), for: .touchUpInside)
+        
+        NSLayoutConstraint.activate([
+            stopButton.topAnchor.constraint(equalTo: cardImageView.bottomAnchor, constant: 30),
+            stopButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            stopButton.widthAnchor.constraint(equalToConstant: 260),
+            stopButton.heightAnchor.constraint(equalToConstant: 50)
+        ])
+    }
+    
+    
+    private func configureResetButton() {
+        view.addSubview(resetButton)
+        resetButton.addTarget(self, action: #selector(resetTimer), for: .touchUpInside)
+        
+        NSLayoutConstraint.activate([
+            resetButton.topAnchor.constraint(equalTo: stopButton.bottomAnchor, constant: 20),
+            resetButton.leadingAnchor.constraint(equalTo: stopButton.leadingAnchor),
+            resetButton.widthAnchor.constraint(equalToConstant: 115),
+            resetButton.heightAnchor.constraint(equalToConstant: 50)
+        ])
+    }
+    
+    
+    private func configureRulesButton() {
+        view.addSubview(rulesButton)
+        rulesButton.addTarget(self, action: #selector(presentRulesVC), for: .touchUpInside)
+        
+        NSLayoutConstraint.activate([
+            rulesButton.topAnchor.constraint(equalTo: stopButton.bottomAnchor, constant: 20),
+            rulesButton.trailingAnchor.constraint(equalTo: stopButton.trailingAnchor),
+            rulesButton.widthAnchor.constraint(equalToConstant: 115),
+            rulesButton.heightAnchor.constraint(equalToConstant: 50)
+        ])
+    }
+    
+    
+    @objc func presentRulesVC() {
+        present(RulesVC(), animated: true)
     }
 }
